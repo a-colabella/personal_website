a9645995d6ee66c25b56bd76ba7525dab6b7abfd
root <acolabella11@gmail.com>
2019-07-25 22:17:11 -0700

    MenuPanel added

---
+++ andrew/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <MenuPanel v-bind:options="menu_items"/>
+  </div>
+</template>
+
+<script>
+import MenuPanel from './components/MenuPanel.vue'
+
+export default {
+  name: 'app',
+  components: { MenuPanel},
+  data() {
+    return {
+      myView: "home",
+      menu_items: [
+        {
+          name: "ABOUT",
+          id: "about"
+        },
+        {
+          name: "SHORT STORIES",
+          id: "shorts"
+        },
+        {
+          name: "PHOTOS",
+          id: "photos"
+        },
+        {
+          name: "RESUME",
+          id: "resume"
+        }]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Cantarell", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ andrew/src/components/MenuPanel.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="menu_panel">
+    <div id="name">
+      ANDREW COLABELLA
+    </div>
+    <ul class="side-items">
+      <li v-bind:id="op.id" v-for="op in options" v-on:click="activate = op.id"
+      v-bind:class="{'activePanelLink': activate == op.id}">
+        {{op.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuPanel',
+  props: ["options"],
+  data() {
+    return {
+      activate: true
+    }
+  },
+  methods: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#menu_panel {
+    background-color: black;
+    position: sticky;
+    z-index: 1;
+    color: white;
+    width: 23%;
+    margin-left: 6%;
+    margin-top: 8%;
+    padding-left: 10px;
+    padding-top: 10px;
+    padding-bottom: 5px;
+}
+
+#name {
+    font-size: 20px;
+    font-weight: bold;
+}
+
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+}
+
+ul li {
+    margin-bottom: 10%;
+    cursor: pointer;
+    width: 50%;
+}
+
+ul li:hover {
+    font-weight: bold;
+}
+
+.activePanelLink {
+    border: solid white 2px;
+}
+
+</style>
